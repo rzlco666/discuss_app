46ebc37c04fae42351e5b3ce543bdab7320b5ff2
Rizal <syahrizalhanif@gmail.com>
2022-12-24 20:57:37 +0700

    API route

---
+++ lib/config/api.dart
@@ -0,0 +1,9 @@
+class Api {
+  static const String _baseUrl = 'http://192.168.1.11/api_discuss';
+  static const String comment = '$_baseUrl/comment';
+  static const String follow = '$_baseUrl/follow';
+  static const String topic = '$_baseUrl/topic';
+  static const String user = '$_baseUrl/user';
+
+  static const String imageUser = '$_baseUrl/image';
+}
