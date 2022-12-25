eb486d7dd1a9e3e7f23d9c7c74b7736aee3f82a6
Rizal <syahrizalhanif@gmail.com>
2022-12-25 15:47:46 +0700

    Init route config in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:discuss_app/config/app_color.dart';
+import 'package:discuss_app/config/app_route.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         primaryColor: AppColor.primary,
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
           primary: AppColor.primary,
         ),
       ),
-      home: Scaffold(),
+      routerConfig: AppRoute.routerConfig,
     );
   }
 }
