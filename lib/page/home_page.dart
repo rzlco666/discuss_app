194d1ba04dde7fe507ee9fc91b3f010aa621ac12
Rizal <syahrizalhanif@gmail.com>
2022-12-25 15:47:15 +0700

    Create route page

---
+++ lib/page/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:d_view/d_view.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: DView.appBarCenter('Home'),
+    );
+  }
+}
