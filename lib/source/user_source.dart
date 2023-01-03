84a6dfba6eac648fd6565dafaa4bcf37132ba4be
Rizal <syahrizalhanif@gmail.com>
2023-01-03 12:21:20 +0700

    Source user
    Create user source, dont forget to always use try catch

---
+++ lib/source/user_source.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:d_method/d_method.dart';
+import 'package:discuss_app/config/api.dart';
+import 'package:http/http.dart';
+
+class UserSource {
+  static Future<Map> register(String username, String password) async {
+    String url = '${Api.user}/register.php';
+    try {
+      Response response = await Client().post(
+        Uri.parse(url),
+        body: {
+          'username': username,
+          'password': password,
+        },
+      );
+      DMethod.printTitle('User Source - register', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody;
+    } catch (e) {
+      DMethod.printTitle('User Source - register', e.toString());
+      return {
+        'success': false,
+      };
+    }
+  }
+}
