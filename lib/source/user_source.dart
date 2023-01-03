2a1960b2f48ccb347e1cadcadef52de5b4d88d2e
Rizal <syahrizalhanif@gmail.com>
2023-01-03 12:41:26 +0700

    Source user login

--- lib/source/user_source.dart
+++ lib/source/user_source.dart
@@ -25,4 +25,25 @@ class UserSource {
       };
     }
   }
+
+  static Future<Map> login(String username, String password) async {
+    String url = '${Api.user}/login.php';
+    try {
+      Response response = await Client().post(
+        Uri.parse(url),
+        body: {
+          'username': username,
+          'password': password,
+        },
+      );
+      DMethod.printTitle('User Source - login', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody;
+    } catch (e) {
+      DMethod.printTitle('User Source - login', e.toString());
+      return {
+        'success': false,
+      };
+    }
+  }
 }
