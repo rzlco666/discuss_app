449dbecdbe69c61a224fe7f90f751c53dde82f79
Rizal <syahrizalhanif@gmail.com>
2023-01-05 20:43:39 +0700

    Source follow read follower and no following

--- lib/source/follow_source.dart
+++ lib/source/follow_source.dart
@@ -4,6 +4,8 @@ import 'package:d_method/d_method.dart';
 import 'package:discuss_app/config/api.dart';
 import 'package:http/http.dart';
 
+import '../model/user.dart';
+
 class FollowSource {
   static Future<bool> checkIsFollowing(
       String fromIdUser, String toIdUser) async {
@@ -41,6 +43,50 @@ class FollowSource {
     } catch (e) {
       DMethod.printTitle('Follow Source - following', e.toString());
       return false;
+    }
+  }
+
+  static Future<bool> noFollowing(String fromIdUser, String toIdUser) async {
+    String url = '${Api.follow}/no_following.php';
+    try {
+      Response response = await Client().post(
+        Uri.parse(url),
+        body: {
+          'from_id_user': fromIdUser,
+          'to_id_user': toIdUser,
+        },
+      );
+      DMethod.printTitle('Follow Source - noFollowing', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody['success'];
+    } catch (e) {
+      DMethod.printTitle('Follow Source - noFollowing', e.toString());
+      return false;
+    }
+  }
+
+  static Future<List<User>> readFollower(String idUser) async {
+    String url = '${Api.follow}/read_follower.php';
+    try {
+      Response response = await Client().post(
+        Uri.parse(url),
+        body: {
+          'id_user': idUser,
+        },
+      );
+      DMethod.printTitle('Follow Source - readFollower', response.body);
+      Map responseBody = jsonDecode(response.body);
+      if (responseBody['success']) {
+        List list = responseBody['data'];
+        return list.map((e) {
+          Map<String, dynamic> item = Map<String, dynamic>.from(e);
+          return User.fromJson(item);
+        }).toList();
+      }
+      return [];
+    } catch (e) {
+      DMethod.printTitle('Follow Source - readFollower', e.toString());
+      return [];
     }
   }
 }
