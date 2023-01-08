bb6aae674470adb3a48f73171aeb54b360b6cd88
Rizal <syahrizalhanif@gmail.com>
2023-01-08 19:17:29 +0700

    Register add topic and profile route

--- lib/config/app_route.dart
+++ lib/config/app_route.dart
@@ -1,6 +1,10 @@
 import 'package:discuss_app/config/session.dart';
+import 'package:discuss_app/controller/c_add_topic.dart';
+import 'package:discuss_app/controller/c_profile.dart';
 import 'package:discuss_app/page/error_page.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 import '../model/user.dart';
 import '../page/home_page.dart';
@@ -11,6 +15,8 @@ class AppRoute {
   static const home = '/';
   static const login = '/login';
   static const register = '/register';
+  static const addTopic = '/add-topic';
+  static const profile = '/profile';
 
   static GoRouter routerConfig = GoRouter(
       errorBuilder: (context, state) => ErrorPage(
@@ -44,6 +50,20 @@ class AppRoute {
         GoRoute(
           path: register,
           builder: (context, state) => const RegisterPage(),
+        ),
+        GoRoute(
+          path: addTopic,
+          builder: (context, state) => ChangeNotifierProvider(
+              create: (_) => CAddTopic(),
+              child: const Scaffold()
+          ),
+        ),
+        GoRoute(
+          path: profile,
+          builder: (context, state) => ChangeNotifierProvider(
+              create: (_) => CProfile(),
+              child: const Scaffold()
+          ),
         ),
       ]);
 }
