0edfd9a5ebba59dcf88bc3615795973660376e05
Rizal <syahrizalhanif@gmail.com>
2023-01-08 20:14:32 +0700

    Controller search

---
+++ lib/controller/c_search.dart
@@ -0,0 +1,38 @@
+import 'package:discuss_app/model/topic.dart';
+import 'package:discuss_app/model/user.dart';
+import 'package:discuss_app/source/topic_source.dart';
+import 'package:discuss_app/source/user_source.dart';
+import 'package:flutter/foundation.dart';
+
+class CSearch extends ChangeNotifier {
+  List<String> get filters => ['Topic', 'User'];
+
+  String _filter = 'Topic';
+  String get filter => _filter;
+  set filter(String newFilter) {
+    _filter = newFilter;
+    notifyListeners();
+  }
+
+  search(String query) {
+    if (filter == 'Topic') {
+      setTopics(query);
+    } else {
+      setUsers(query);
+    }
+  }
+
+  List<Topic> _topics = [];
+  List<Topic> get topics => _topics;
+  setTopics(String query) async {
+    _topics = await TopicSource.search(query);
+    notifyListeners();
+  }
+
+  List<User> _users = [];
+  List<User> get users => _users;
+  setUsers(String query) async {
+    _users = await UserSource.search(query);
+    notifyListeners();
+  }
+}
