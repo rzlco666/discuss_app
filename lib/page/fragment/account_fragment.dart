c3452ae39ac612390f050e301f2b7e4ef4cf2dec
Rizal <syahrizalhanif@gmail.com>
2023-01-20 19:09:26 +0700

    Account page 2
    - Create stats
    - Create button logout
    - Fix layout

--- lib/page/fragment/account_fragment.dart
+++ lib/page/fragment/account_fragment.dart
@@ -1,5 +1,6 @@
 import 'package:d_view/d_view.dart';
 import 'package:discuss_app/config/api.dart';
+import 'package:discuss_app/config/app_format.dart';
 import 'package:discuss_app/controller/c_account.dart';
 import 'package:discuss_app/controller/c_user.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,12 +10,14 @@ import 'package:provider/provider.dart';
 class AccountFragment extends StatelessWidget {
   const AccountFragment({super.key});
 
+  logout(BuildContext context) {}
+
   @override
   Widget build(BuildContext context) {
     var cUser = context.read<CUser>();
     var cAccount = context.read<CAccount>();
 
-    if(cUser.data == null) return DView.empty();
+    if (cUser.data == null) return DView.empty();
     cAccount.setStat(cUser.data!.id);
 
     double widthBoxImage = MediaQuery.of(context).size.width * 0.5;
@@ -27,48 +30,50 @@ class AccountFragment extends StatelessWidget {
           child: DView.textTitle('Account', size: 20),
         ),
         DView.spaceHeight(30),
-        SizedBox(
-          width: widthBoxImage,
-          height: widthBoxImage,
-          child: Stack(
-            children: [
-              Container(
-                width: widthBoxImage,
-                height: widthBoxImage,
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    width: 4,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black38,
-                      blurRadius: 6,
-                      offset: Offset(2, 3),
+        Center(
+          child: SizedBox(
+            width: widthBoxImage,
+            height: widthBoxImage,
+            child: Stack(
+              children: [
+                Container(
+                  width: widthBoxImage,
+                  height: widthBoxImage,
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      width: 4,
+                      color: Theme.of(context).primaryColor,
                     ),
-                  ],
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black38,
+                        blurRadius: 6,
+                        offset: Offset(2, 3),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: Material(
-                  elevation: 8,
-                  borderRadius: BorderRadius.circular(90),
-                  child: ClipRRect(
+                Align(
+                  alignment: Alignment.center,
+                  child: Material(
+                    elevation: 8,
                     borderRadius: BorderRadius.circular(90),
-                    child: Image.network(
-                      '${Api.imageUser}/${cUser.data!.image}',
-                      fit: BoxFit.cover,
-                      width: widthBoxImage - 20,
-                      height: widthBoxImage - 20,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(90),
+                      child: Image.network(
+                        '${Api.imageUser}/${cUser.data!.image}',
+                        fit: BoxFit.cover,
+                        width: widthBoxImage - 20,
+                        height: widthBoxImage - 20,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
         DView.spaceHeight(12),
@@ -76,21 +81,87 @@ class AccountFragment extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+              height: 30,
+              padding: const EdgeInsets.symmetric(horizontal: 8),
               decoration: BoxDecoration(
                 color: Colors.grey.withOpacity(0.8),
                 borderRadius: BorderRadius.circular(4),
               ),
+              alignment: Alignment.center,
               child: DView.textTitle(cUser.data!.username, color: Colors.white),
             ),
             DView.spaceWidth(),
-            ElevatedButton.icon(
-                onPressed: () {},
-                icon: const Icon(Icons.edit, size: 14),
-                label: const Text('Image')
+            SizedBox(
+              height: 30,
+              child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.edit, size: 14),
+                  label: const Text('Image')),
             ),
           ],
+        ),
+        Consumer<CAccount>(
+          builder: (contextConsumer, _, child) {
+            return Padding(
+              padding: const EdgeInsets.all(16),
+              child: Material(
+                elevation: 8,
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(4),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: itemStat('Topic', _.stat['topic']),
+                      ),
+                      Container(
+                        width: 0.5,
+                        height: 30,
+                        color: Colors.grey,
+                      ),
+                      Expanded(
+                        child: itemStat('Follower', _.stat['follower']),
+                      ),
+                      Container(
+                        width: 0.5,
+                        height: 30,
+                        color: Colors.grey,
+                      ),
+                      Expanded(
+                        child: itemStat('Following', _.stat['following']),
+                      ),
+                      Container(
+                        width: 0.5,
+                        height: 30,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+        DView.spaceHeight(8),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          width: double.infinity,
+          child: OutlinedButton(
+            onPressed: () => logout(context),
+            child: const Text('Logout'),
+          ),
         )
+      ],
+    );
+  }
+
+  Widget itemStat(String title, num value) {
+    return Column(
+      children: [
+        Text(title),
+        DView.spaceHeight(4),
+        DView.textTitle(AppFormat.infoNumber(value.toDouble())),
       ],
     );
   }
